257b245d907e620327e8610193abec31d5f82ab9
maksutovaynur <maksutov_aynur@mail.ru>
2020-09-20 03:13:13 +0300

    fix bug

--- tgbot/src/fill_data.py
+++ tgbot/src/fill_data.py
@@ -4,9 +4,9 @@ from . import config as S
 from os.path import join
 
 
-def insert_or_create(col, row):
+def insert_or_create(col, row, idx):
     data = dict(**row._asdict())
-    data["Index"] = f"{i}_{data['Index']}"
+    data["Index"] = f"{idx}_{data['Index']}"
     col.update_one(
         {"Index": data["Index"]},
         update={"$set": data},
@@ -22,7 +22,7 @@ _LOG_EVERY = 500
 print("Balance info")
 df = pd.read_csv(join(S.ROOT_DATA_FOLDER, "avk_hackathon_data_account_x_balance.csv"))
 for row in df.itertuples():
-    insert_or_create(db.balances, row)
+    insert_or_create(db.balances, row, 0)
     rows_written += 1
     if rows_written % _LOG_EVERY == 0: print(f"{rows_written} rows written")
 
@@ -30,7 +30,7 @@ for row in df.itertuples():
 print("Socdem info")
 df = pd.read_csv(join(S.ROOT_DATA_FOLDER, "avk_hackathon_data_party_x_socdem.csv"))
 for row in df.itertuples():
-    insert_or_create(db.socdem, row)
+    insert_or_create(db.socdem, row, 0)
     rows_written += 1
     if rows_written % _LOG_EVERY == 0: print(f"{rows_written} rows written")
 
@@ -39,7 +39,7 @@ print("Start write transactions")
 for i, fn in enumerate(join(S.ROOT_DATA_FOLDER, f"avk_hackathon_data_transactions-{j}.csv") for j in range(1, 1 + _MAX_PARTS)):
     df = pd.read_csv(fn, parse_dates=["transaction_dttm"])
     for row in df.itertuples():
-        insert_or_create(db.transactions, row)
+        insert_or_create(db.transactions, row, i)
         rows_written += 1
         if rows_written % _LOG_EVERY == 0: print(f"{rows_written} rows written")
 
